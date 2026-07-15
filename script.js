b3f2e93475102adf39800e8b4c05470a25e9c1da
github <diyorbekabdumominov69@gmail.com>
2026-07-15 17:57:51 +0500

    Add event listener for Google Register button

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const registerBtn = document.getElementById("registerBtn");
+
+    registerBtn.addEventListener("click", () => {
+        alert("Google Register tez orada qo'shiladi.");
+    });
+
+});
